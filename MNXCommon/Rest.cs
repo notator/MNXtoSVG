0c6a76355f1773b03dbb40bff13d292d1ae23aab
notator <j.ingram@netcologne.de>
2020-04-16 17:26:31 +0200

    Changes to Rest class definition.

--- MNXCommon/Rest.cs
+++ MNXCommon/Rest.cs
@@ -5,14 +5,29 @@ using System.Xml;
 namespace MNX.Common
 {
     // https://w3c.github.io/mnx/specification/common/#elementdef-rest
-    public class Rest : ITicks, IEventComponent
+    // Rest is a *field* in Event.
+    public class Rest
     {
+        public readonly string Pitch;
+
         public Rest(XmlReader r)
         {
             A.Assert(r.Name == "rest");
+
+            int count = r.AttributeCount;
+            for(int i = 0; i < count; i++)
+            {
+                r.MoveToAttribute(i);
+                switch(r.Name)
+                {
+                    case "pitch":
+                        Pitch = r.Value;
+                        break;
+                }
+            }
+            A.ReadToXmlElementTag(r, "rest");
+
+            A.Assert(r.Name == "rest"); // end of rest
         }
-
-        public int Ticks => throw new NotImplementedException();
-
     }
 }

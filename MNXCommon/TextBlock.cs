a2599b3a6633ef9f7207a919ce2b2c6f9faa5019
notator <j.ingram@netcologne.de>
2021-12-16 15:15:16 +0100

    <text-block> can now contain <p> and <div> elements...
    <simple-text-block> has also been created (like <div>).

--- MNXCommon/TextBlock.cs
+++ MNXCommon/TextBlock.cs
@@ -1,36 +1,60 @@
 ﻿using MNX.Globals;
 
+using System.Collections.Generic;
 using System.Xml;
+
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 namespace MNX.Common
 {
     public class TextBlock : IDirectionsComponent
     {
-        public readonly string Text;
+        public readonly string Lines = "";
         public readonly int TicksPosInScore = -1; // set in ctor
 
         public TextBlock(XmlReader r, int ticksPosInScore)
         {
-            M.Assert(r.Name == "text-block");
-
-            XmlReaderSettings localSettings = new XmlReaderSettings();
-            localSettings.DtdProcessing = DtdProcessing.Parse;
-
-            TicksPosInScore = ticksPosInScore;
-
-            using(XmlReader htmlReader = XmlReader.Create(r, localSettings))
+            // returns all XmlNodes whose name is nodeName in the document tree
+            List<XmlNode> GetNodesByName(XmlNode rootNode, string nodeName)
             {
-                htmlReader.Read();
-                Text = htmlReader.Value;
+                List<XmlNode> xmlNodes = new List<XmlNode>();
+
+                void RecursiveGetNodes(XmlNode baseNode)
+                {
+                    if(baseNode.ChildNodes.Count > 0)
+                    {
+                        foreach(XmlNode childNode in baseNode.ChildNodes)
+                        {
+                            if(childNode.Name == nodeName)
+                            {
+                                xmlNodes.Add(childNode);
+                            }
+                            RecursiveGetNodes(childNode);
+                        }  
+                    }
+                }
+
+                RecursiveGetNodes(rootNode);
+
+                return xmlNodes;
             }
 
-            //r.Read();
-            //Text = r.Value;
+            M.Assert(r.Name == "text-block");
+
+            string filePath = r.BaseURI;
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePath);
+
+            XmlNode root = doc.DocumentElement;
+            List<XmlNode> pNodes = GetNodesByName(root, "p");
+            List<XmlNode> divNodes = GetNodesByName(root, "div");
+            List<XmlNode> textBlockNodes = GetNodesByName(root, "text-block");
+            List<XmlNode> simpleTextBlockNodes = GetNodesByName(root, "simple-text-block");
 
             M.ReadToXmlElementTag(r, "text-block");
 
-
-            M.Assert(r.Name == "text-block"); // end of "directions"
+            M.Assert(r.Name == "text-block"); // end of "text-block"
         }
     }
 }

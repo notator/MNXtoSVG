2fe8e4662065e344cc21a7bd7ee4e73d6a6a5b7e
notator <j.ingram@netcologne.de>
2021-12-25 13:02:46 +0100

    Minor Fix: (forgotten correction to CurrentTupletlevel.cs)

--- MNXCommon/CurrentTupletLevel.cs
+++ MNXCommon/CurrentTupletLevel.cs
@@ -14,13 +14,5 @@ namespace MNX.Common
         /// and decremented when it ends. 
         /// </summary>
         internal static int CurrentTupletLevel = 0;
-
-        /// <summary>
-        /// Used while parsing.
-        /// The value 0 means that there are no beams currently active.
-        /// This value is incremented at the beginning of a Beam constructor,
-        /// and decremented when it ends. 
-        /// </summary>
-        internal static int CurrentBeamLevel = 0;
     }
 }
